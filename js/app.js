c5b6e9f771fe4afa1aaa5b7dfb9e5214b80d2933
Leandro Blandi <leandronahuelblandiomg@gmail.com>
2022-10-13 13:36:28 -0300

    Added validation client-side JS for Netlify Form

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 
+const form = document.querySelector(".contact-form__form");
 
+// Cuando cargue el documento
 document.addEventListener("DOMContentLoaded", ()=> {
   
   new TypeIt(".autotype",
@@ -16,6 +18,33 @@ document.addEventListener("DOMContentLoaded", ()=> {
   ).go();
 });
 
+// Cuando se intente realizar un submit del formulario
+form.addEventListener("submit", (event)=> {
+  
+  event.preventDefault();
 
+  let name = document.querySelector("#senderName");
+  let subject = document.querySelector("#senderSubject");
+  let message = document.querySelector("#senderMessage");
 
+  // Si los campos estan vacios
+  if(name.value === "") {
+    window.alert("Oops! Your name is required. Type it :)");
+    return;
+  }
 
+  if(subject.value === "") {
+    window.alert("Oops! What's the subject of your message? Type it :-)");
+    return;
+  }
+
+  if(message.value === "") {
+    window.alert("Oops! The message is empty. Write something :-)");
+    return;
+  }
+
+  // De lo contrario que se envie el formulario
+  if((name.value != "") && (subject.value != "") && (message.value != "")) {
+    form.submit();
+  }
+});
