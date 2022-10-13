1a6259dd3fc996508dd927f53d9009a3d35334d8
Leandro Blandi <leandronahuelblandiomg@gmail.com>
2022-10-13 13:46:19 -0300

    Refactoring the code into functions, added e-mail validation

--- js/app.js
+++ js/app.js
@@ -1,9 +1,48 @@
 
 const form = document.querySelector(".contact-form__form");
 
-// Cuando cargue el documento
-document.addEventListener("DOMContentLoaded", ()=> {
-  
+
+function validateEmail(email) {
+  let re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
+
+function validateForm() {
+
+  let name = document.querySelector("#senderName");
+  let mail = document.querySelector("#senderSubject");
+  let message = document.querySelector("#senderMessage");
+
+  // Si los campos estan vacios
+  if(name.value === "") {
+    window.alert("Oops! Your name is required. Type it :)");
+    return;
+  }
+
+  if(mail.value === "") {
+    window.alert("Oops! What's your E-Mail? Type it :-)");
+    return;
+  }
+
+  if(!validateEmail(mail.value)) {
+    window.alert(`Seems that '${mail.value}' isn't valid. Check another :-(`);
+  }
+
+  if(message.value === "") {
+    window.alert("Oops! The message is empty. Write something :-)");
+    return;
+  }
+
+  // De lo contrario que se envie el formulario
+  if((name.value != "") && (mail.value != "") && (validateEmail(mail.value)) && (message.value != "")) {
+    form.submit();
+  }
+}
+
+
+function startTypingDescription() {
+
   new TypeIt(".autotype",
   {
     strings: ["Trainee Developer", "Software Development Student"],
@@ -16,35 +55,22 @@ document.addEventListener("DOMContentLoaded", ()=> {
     breakLines: false
   }
   ).go();
+}
+
+
+// Cuando cargue el documento
+document.addEventListener("DOMContentLoaded", ()=> {
+
+  startTypingDescription();
+
 });
+
 
 // Cuando se intente realizar un submit del formulario
 form.addEventListener("submit", (event)=> {
   
   event.preventDefault();
 
-  let name = document.querySelector("#senderName");
-  let subject = document.querySelector("#senderSubject");
-  let message = document.querySelector("#senderMessage");
+  validateForm();
 
-  // Si los campos estan vacios
-  if(name.value === "") {
-    window.alert("Oops! Your name is required. Type it :)");
-    return;
-  }
-
-  if(subject.value === "") {
-    window.alert("Oops! What's the subject of your message? Type it :-)");
-    return;
-  }
-
-  if(message.value === "") {
-    window.alert("Oops! The message is empty. Write something :-)");
-    return;
-  }
-
-  // De lo contrario que se envie el formulario
-  if((name.value != "") && (subject.value != "") && (message.value != "")) {
-    form.submit();
-  }
 });
